edec66ea4573025bc530ec5ab6162ae8a1455e4d
Kenneth Henderick <kenneth@ketronic.be>
2019-07-31 10:41:41 +0200

    Added logic for representing EEPROM/FRAM + Added tooling to read from it + Added models for some configurations

---
+++ src/master_aio/memory_file.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2019 OpenMotics BVBA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Contains a memory representation
+"""
+import logging
+from master_aio.aio_api import AIOAPI
+
+LOGGER = logging.getLogger("openmotics")
+
+
+class MemoryTypes(object):
+    FRAM = 'F'
+    EEPROM = 'E'
+
+
+class MemoryFile(object):
+
+    def __init__(self, memory_type, aio_communicator):
+        """
+        Initializes the MemoryFile instance, reprensenting one of the supported memory types
+
+        :type aio_communicator: master_aio.aio_communicator.AIOCommunicator
+        """
+
+        self._aio_communicator = aio_communicator
+        self.type = memory_type
+        if memory_type == MemoryTypes.EEPROM:
+            self._pages = 512
+            self._page_length = 256
+        elif memory_type == MemoryTypes.FRAM:
+            self._pages = 128
+            self._page_length = 256
+        self._cache = {}
+
+    def read(self, addresses):
+        """
+        :type addresses: list of master_aio.memory_types.MemoryAddress
+        """
+        data = {}
+        for address in addresses:
+            page_data = self.read_page(address.page)
+            data[address] = page_data[address.offset:address.offset + address.length]
+        return data
+
+    def read_page(self, page):
+        if page not in self._cache:
+            page_data = []
+            for i in xrange(self._page_length / 32):
+                page_data += self._aio_communicator.do_command(
+                    AIOAPI.memory_read(),
+                    {'type': self.type, 'page': page, 'start': i * 32, 'length': 32}
+                )['data']
+            self._cache[page] = page_data
+        return self._cache[page]
+
+    def write_page(self, page, data):
+        self._cache[page] = data
+        for i in xrange(self._page_length / 32):
+            self._aio_communicator.do_command(
+                AIOAPI.memory_write(32),
+                {'type': self.type, 'page': page, 'start': i * 32, 'data': data}
+            )
+
+    def invalidate_cache(self, page=None):
+        pages = [page]
+        if page is None:
+            pages = range(self._pages)
+        for page in pages:
+            self._cache.pop(page, None)
---
+++ src/master_aio/memory_models.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2019 OpenMotics BVBA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Memory models
+"""
+from master_aio.memory_types import MemoryModelDefinition, MemoryRelation, MemoryByteField, MemoryWordField, MemoryByteArrayField, MemoryAddressField, MemoryStringField
+from master_aio.memory_file import MemoryTypes
+
+
+class GlobalConfiguration(MemoryModelDefinition):
+    number_of_output_modules = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 1))
+    number_of_input_modules = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 2))
+    number_of_sensor_modules = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 3))
+    scan_time_rs485_modules = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 4))
+    number_of_can_sensors = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 6))
+    number_of_ucan_modules = MemoryByteField(MemoryTypes.EEPROM, address_spec=(0, 7))
+    groupaction_all_outputs_off = MemoryWordField(MemoryTypes.EEPROM, address_spec=(0, 50))
+    groupaction_startup = MemoryWordField(MemoryTypes.EEPROM, address_spec=(0, 52))
+    groupaction_minutes_changed = MemoryWordField(MemoryTypes.EEPROM, address_spec=(0, 54))
+    groupaction_hours_changed = MemoryWordField(MemoryTypes.EEPROM, address_spec=(0, 56))
+    groupaction_day_changed = MemoryWordField(MemoryTypes.EEPROM, address_spec=(0, 58))
+
+
+class OutputModuleConfiguration(MemoryModelDefinition):
+    device_type = MemoryStringField(MemoryTypes.EEPROM, address_spec=lambda id: (id + 1, 0), length=1)
+    address = MemoryAddressField(MemoryTypes.EEPROM, address_spec=lambda id: (id + 1, 0))
+    firmware_version = MemoryByteArrayField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id, 4), length=3)
+
+
+class OutputConfiguration(MemoryModelDefinition):
+    timer_value = MemoryWordField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 7 + id % 8))
+    timer_type = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 23 + id % 8))
+    output_type = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 31 + id % 8))
+    min_output_level = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 39 + id % 8))
+    max_output_level = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 47 + id % 8))
+    output_groupaction_follow = MemoryWordField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 55 + (id % 8) * 2))
+    shutter_up_timer = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 64 + (id % 8) * 2))
+    shutter_down_timer = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 65 + (id % 8) * 2))
+    shutter_config = MemoryByteField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 72 + id % 8))
+    name = MemoryStringField(MemoryTypes.EEPROM, address_spec=lambda id: (1 + id / 8, 128 + (id % 8) * 16), length=16)
+    module = MemoryRelation(OutputModuleConfiguration, id_spec=lambda id: id / 8)
