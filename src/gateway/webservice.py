76d3b7b3ff90ad56ba5fb11a6725edff251cdc7f
Kenneth Henderick <kenneth@ketronic.be>
2019-11-22 09:59:38 +0100

    Version bump

--- src/gateway/webservice.py
+++ src/gateway/webservice.py
@@ -2228,7 +2228,7 @@ class WebInterface(object):
         :rtype: dict
         """
         return {'version': self._gateway_api.get_main_version(),
-                'gateway': '2.12.4'}
+                'gateway': '2.13.0'}
 
     @openmotics_api(auth=True, plugin_exposed=False)
     def update(self, version, md5, update_data):
