fecf68918037944372ce0c13da73e48d67219cf3
Sander Van Schoote <vanschoote.sander@gmail.com>
2019-11-26 17:16:51 +0100

    Fixed event serializing

--- src/plugins/runner.py
+++ src/plugins/runner.py
@@ -151,7 +151,7 @@ class PluginRunner:
                         self.logger('[Runner] Exception during killing plugin: {0}'.format(exception))
 
     def process_input_status(self, input_event):
-        event_json = input_event.deserialize()
+        event_json = input_event.serialize()
         self._do_async('input_status', {'event': event_json}, should_filter=True)
 
     def process_output_status(self, status):
