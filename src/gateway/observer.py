a0d8c29c2671d6605051c362916f2f767590a3b4
Sander Van Schoote <vanschoote.sander@gmail.com>
2019-12-05 09:37:20 +0100

    optimizations

--- src/gateway/observer.py
+++ src/gateway/observer.py
@@ -309,9 +309,10 @@ class Observer(object):
 
     def _refresh_inputs(self):
         """ Refreshes the Input Status tracker """
-        input_configurations = self._gateway_api.get_input_configurations()
-        for input_configuration in input_configurations:
-            self._input_config[input_configuration['id']] = input_configuration
+        # 1. refresh input configuration
+        self._input_config = {input_configuration['id']: input_configuration for input_configuration in self._gateway_api.get_input_configurations()}
+
+        # 2. poll for latest input status
         try:
             number_of_input_modules = self._master_communicator.do_command(master_api.number_of_io_modules())['in']
             inputs = []
