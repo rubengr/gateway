de74d1adcc3aa301beb567fcb91854c93964dee4
Sander Van Schoote <vanschoote.sander@gmail.com>
2019-12-30 18:03:57 +0100

    Only load eggs in path, not whl

--- src/platform_utils.py
+++ src/platform_utils.py
@@ -212,11 +212,9 @@ class System(object):
         os.environ['PYTHON_EGG_CACHE'] = '/tmp/.eggs-cache/'
 
         libs = (glob.glob('{0}/eggs/*.egg'.format(current_file_path)) +
-                glob.glob('{0}/eggs/{1}/*.egg'.format(current_file_path, operating_system)) +
-                glob.glob('{0}/eggs/*.whl'.format(current_file_path)) +
-                glob.glob('{0}/eggs/{1}/*.whl'.format(current_file_path, operating_system)))
+                glob.glob('{0}/eggs/{1}/*.egg'.format(current_file_path, operating_system)))
         for lib in libs:
-            logger.info('Loading lib... {}'.format(lib))
+            logger.info('Loading egg... {}'.format(lib))
             sys.path.insert(0, lib)
 
         # Patching where/if required
