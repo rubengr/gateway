35b1e21375d9677c3a6bca8cf3a3370f41fd4a59
Kenneth Henderick <kenneth@ketronic.be>
2020-01-07 17:09:37 +0100

    OpenMotics BVBA -> OpenMotics BV

--- src/gateway/observer.py
+++ src/gateway/observer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 OpenMotics BVBA
+# Copyright (C) 2018 OpenMotics BV
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
