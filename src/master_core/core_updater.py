9be36641a9cfeb67885e7f08922bbee37e021f3d
Kenneth Henderick <kenneth@ketronic.be>
2020-01-22 14:35:23 +0100

    Fixing a few issues, update code now working

--- src/master_core/core_updater.py
+++ src/master_core/core_updater.py
@@ -31,6 +31,7 @@ class CoreUpdater(object):
     """
 
     BOOTLOADER_SERIAL_READ_TIMEOUT = 3
+    RESET_DELAY = 2
 
     @staticmethod
     @Inject
@@ -38,8 +39,8 @@ class CoreUpdater(object):
         """
         Flashes the content from an Intel HEX file to the Core
         :param hex_filename: The filename of the hex file to flash
-        :type core_communicator: master_core.core_communicator.CoreCommunicator
-        :type maintenance_communicator: master_core.maintenance.MaintenanceCommunicator
+        :type core_communicator: master_core.core_communicator.CoreCommunicator or None
+        :type maintenance_communicator: master_core.maintenance.MaintenanceCommunicator or None
         :type cli_serial: serial.Serial
         """
         try:
@@ -47,8 +48,9 @@ class CoreUpdater(object):
 
             logger.info('Updating Core')
 
-            maintenance_communicator.stop()
-            # core_communicator.stop()  # TODO: Hold the communicator
+            if core_communicator is not None and maintenance_communicator is not None:
+                maintenance_communicator.stop()
+                # core_communicator.stop()  # TODO: Hold the communicator
 
             if not os.path.exists(hex_filename):
                 raise RuntimeError('The given path does not point to an existing file')
@@ -56,45 +58,45 @@ class CoreUpdater(object):
             with open(hex_filename, 'r') as hex_file:
                 hex_lines = hex_file.readlines()
 
-            in_bootloader = False  # TODO: Figure out whether the BL is active
-            if in_bootloader:
-                logger.info('Bootloader active')
+            logger.info('Verify bootloader communications')
+            bootloader_version = CoreUpdater._in_bootloader(cli_serial)
+            if bootloader_version is not None:
+                logger.info('Bootloader {0} active'.format(bootloader_version))
             else:
                 logger.info('Bootloader not active, switching to bootloader')
-                # TODO: Switch to bootloader
-                in_bootloader = True
-                if not in_bootloader:
+                cli_serial.write('reset\r\n')
+                time.sleep(CoreUpdater.RESET_DELAY)
+                bootloader_version = CoreUpdater._in_bootloader(cli_serial)
+                if bootloader_version is None:
                     raise RuntimeError('Could not enter bootloader')
-                logger.info('Bootloader active')
-
-            logger.info('Verify bootloader communications')  # TODO: Probably will be replaced by the above "in bootloader" check
-            cli_serial.write('hi\n')
-            response = CoreUpdater.read_line(cli_serial)
-            if not response.startswith('hi;version='):
-                raise RuntimeError('Unexpected bootloader resposne: {0}'.format(response))
-            logger.info('Bootloader version {0}'.format(response.split('=')[-1]))
+                logger.info('Bootloader {0} active'.format(bootloader_version))
 
             logger.info('Flashing contents of {0}'.format(os.path.basename(hex_filename)))
             logger.info('Flashing...')
             amount_lines = len(hex_lines)
             for index, line in enumerate(hex_lines):
                 cli_serial.write(line)
-                response = CoreUpdater.read_line(cli_serial)
+                response = CoreUpdater._read_line(cli_serial)
                 if response.startswith('nok'):
                     raise RuntimeError('Unexpected NOK while flashing: {0}'.format(response))
                 if not response.startswith('ok'):
                     raise RuntimeError('Unexpected answer while flashing: {0}'.format(response))
                 if index % int(amount_lines / 10) == 0:
-                    logger.debug('* {0}%'.format(int(index * 100 / amount_lines) + 1))
+                    logger.info('Flashing... {0}%'.format(int(index * 100 / amount_lines) + 1))
             logger.info('Flashing... Done')
 
-            # TODO: Figure out how to jump back to application
-
-            maintenance_communicator.start()
-            # core_communicator.start()  # TODO: Make sure it can start again
-
             logger.info('Verify Core communication')
-            # TODO: Execute call to load firmware version
+            time.sleep(CoreUpdater.RESET_DELAY)
+            if CoreUpdater._in_bootloader(cli_serial):
+                raise RuntimeError('Still in bootloader')
+            cli_serial.write('firmware version\r\n')
+            firmware_version = CoreUpdater._read_line(cli_serial, discard_lines=2)
+            logger.info('Application version {0} active'.format(firmware_version))
+            cli_serial.flushInput()
+
+            if core_communicator is not None and maintenance_communicator is not None:
+                maintenance_communicator.start()
+                # core_communicator.start()  # TODO: Make sure it can start again
 
             logger.info('Update completed')
             return True
@@ -103,7 +105,17 @@ class CoreUpdater(object):
             return False
 
     @staticmethod
-    def read_line(serial, verbose=True):
+    def _in_bootloader(serial):
+        serial.flushInput()
+        serial.write('hi\n')
+        response = CoreUpdater._read_line(serial)
+        serial.flushInput()
+        if not response.startswith('hi;ver='):
+            return None
+        return response.split('=')[-1]
+
+    @staticmethod
+    def _read_line(serial, verbose=True, discard_lines=0):
         timeout = time.time() + CoreUpdater.BOOTLOADER_SERIAL_READ_TIMEOUT
         line = ''
         while time.time() < timeout:
@@ -114,6 +126,9 @@ class CoreUpdater(object):
                     if line[0] == '#' and verbose:
                         logger.debug('* Debug: {0}'.format(line.strip()))
                         line = ''
-                    else:
+                    elif discard_lines == 0:
                         return line.strip()
+                    else:
+                        discard_lines -= 1
+                        line = ''
         raise RuntimeError('Timeout while communicating with Core bootloader')
