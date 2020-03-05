441a729cdc294e8bc73520ff1ec76825388ecd15
Daiderd Jordan <djordan@openmotics.com>
2020-03-05 17:12:19 +0100

    add mantenance test
    This currently only works on the local network.

--- testing/cicd/tests/conftest.py
+++ testing/cicd/tests/conftest.py
@@ -11,7 +11,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 settings.register_profile('default', deadline=None, max_examples=10, stateful_step_count=5, print_blob=True)
 settings.register_profile('once', deadline=None, max_examples=1, stateful_step_count=5, print_blob=True)
-settings.register_profile('ci', deadline=None, max_examples=500, stateful_step_count=10, print_blob=True)
+settings.register_profile('ci', deadline=None, max_examples=100, stateful_step_count=10, print_blob=True)
 settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'default'))
 
 
--- testing/cicd/tests/mode_test.py
+++ testing/cicd/tests/mode_test.py
@@ -13,23 +13,26 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
+import socket
+import ssl
 import time
 
+import psutil
 import pytest
 
 logger = logging.getLogger('openmotics')
+
+
+def check_ip_range():
+    # type: () -> bool
+    addresses = ([x.address for x in xs if x.broadcast == '10.91.115.255'] for xs in psutil.net_if_addrs().values())
+    return not sum(addresses, [])
 
 
 @pytest.fixture
 def power_on(request, toolbox):
     yield
     toolbox.ensure_power_on()
-
-
-@pytest.mark.slow
-@pytest.mark.skip(reason='makes other tests unreliable')
-def test_power_cycle(toolbox, power_on):
-    toolbox.power_cycle()
 
 
 @pytest.fixture
@@ -39,6 +42,12 @@ def discover_mode(request, toolbox):
 
 
 @pytest.mark.slow
+@pytest.mark.skip(reason='makes other tests unreliable')
+def test_power_cycle(toolbox, power_on):
+    toolbox.power_cycle()
+
+
+@pytest.mark.smoke
 def test_module_discover(toolbox, discover_mode):
     logger.info('starting module discovery')
     toolbox.target.get('/module_discover_start')
@@ -52,3 +61,38 @@ def test_module_discover(toolbox, discover_mode):
     assert 'I' in data['inputs']
     assert 'outputs' in data
     assert 'O' in data['outputs']
+
+
+@pytest.mark.slow
+@pytest.mark.skipif(check_ip_range(), reason='the maintenance ports are not accessible on jenkins')
+def test_maintenance(toolbox):
+    # TODO: ensure maintenance mode is disabled afterwards
+    data = toolbox.target.get('/get_status')
+    expected_version = 'F{} H{}'.format(data['version'], data['hw_version'])
+
+    logger.info('starting maintenance')
+    data = toolbox.target.get('/open_maintenance')
+    assert 'port' in data
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    time.sleep(2)
+    ssl_sock = ssl.wrap_socket(
+        sock,
+        ssl_version=ssl.PROTOCOL_SSLv23,
+        do_handshake_on_connect=False,
+        suppress_ragged_eofs=False
+    )
+    ssl_sock.connect((toolbox.target._host, data['port']))
+
+    data = ''
+    while data != 'OK':
+        data = ssl_sock.recv(1024).strip()
+        logger.debug('received data "{}"'.format(data))
+
+    ssl_sock.send('firmware version\r\n')
+    assert ssl_sock.recv(1024).strip() == 'firmware version'
+    assert ssl_sock.recv(1024).strip() == expected_version
+    ssl_sock.send('exit\r\n')
+    ssl_sock.close()
