e495686fcb84842e7f21a77e5f7c85a165e5564d
Daiderd Jordan <djordan@openmotics.com>
2020-03-10 13:57:09 +0100

    query syslog receiver plugin and print logs after each test

--- testing/cicd/tests/conftest.py
+++ testing/cicd/tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from hypothesis import settings
 from pytest import fixture
@@ -8,6 +9,8 @@ from .toolbox import Toolbox
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+logger = logging.getLogger('openmotics')
+
 
 settings.register_profile('default', deadline=None, max_examples=10, stateful_step_count=5, print_blob=True)
 settings.register_profile('once', deadline=None, max_examples=1, stateful_step_count=5, print_blob=True)
@@ -16,5 +19,17 @@ settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'default'))
 
 
 @fixture(scope='session')
-def toolbox():
+def toolbox_session():
     return Toolbox()
+
+
+@fixture
+def toolbox(toolbox_session):
+    toolbox = toolbox_session
+    yield toolbox
+    try:
+        data = toolbox.observer.get('/plugins/syslog_receiver/logs')
+        for log in data['logs']:
+            print(log)
+    except Exception:
+        print('Failed to retrieve logs')
